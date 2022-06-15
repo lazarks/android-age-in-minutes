6a8a109b1c71b50a0a01b30e8439c677558f96cc
Lazar <sljivic.lazar037@gmail.com>
2022-06-15 12:41:00 +0200

    style cleanup + select button test

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.ageinminutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSelectDate = findViewById<Button>(R.id.btnSelectDate)
+        btnSelectDate.setOnClickListener{
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            println("clicked!!!")
+        }
     }
 }
