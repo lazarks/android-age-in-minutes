b41740b3e9d27765f6fd5cc6c3281522929488fd
Lazar <sljivic.lazar037@gmail.com>
2022-06-15 14:14:23 +0200

    selected date && selected date in minutes functionalities finished

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +16,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnSelectDate = findViewById<Button>(R.id.btnSelectDate)
-        btnSelectDate.setOnClickListener{
-            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
-            println("clicked!!!")
+        btnSelectDate.setOnClickListener {
+            clickDatePicker()
+        //  Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun clickDatePicker() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, {
+                _, selectedYear, selectedMonth, selectedDay ->
+
+            val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+            Toast.makeText(this@MainActivity, "The chosen date is $selectedDate", Toast.LENGTH_LONG).show()
+
+            val tvSelectedDate = findViewById<TextView>(R.id.tvSelectedDate)
+            tvSelectedDate.text = selectedDate
+
+            val theDate = SimpleDateFormat("dd/MM/yyy", Locale.ENGLISH).parse(selectedDate)
+
+            val diffMillis: Long = myCalendar.timeInMillis - theDate.time
+            val diffMinutes = (diffMillis / 1000) / 60
+
+            val tvSelectedDateInMinutes = findViewById<TextView>(R.id.tvSelectedDateInMinutes)
+            tvSelectedDateInMinutes.text = "$diffMinutes"
+
+        }, year, month, day).show()
     }
 }
